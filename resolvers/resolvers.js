2622dcc12ba537e2959ad30afbb7d10ed473558f
Kashfi <kashfimehbuba71@gmail.com>
2026-02-20 04:32:19 -0500

    updating resolver file

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,3 +1,33 @@
-import MovieModel from '../models/movie.js';
+import MovieModel from '../models/Movie.js';
 
 // Resolvers define the technique for fetching the types defined in the schema.
+
+// graphql/resolvers.js
+const MovieModel = require("../models/Movie.js");
+
+const resolvers = {
+  Query: {
+    // a) Get all movies
+    getAllMovies: () => MovieModel.getAll(),
+
+    // b) Get movie by ID
+    getMovieById: (_, { id }) => MovieModel.getById(id),
+
+    // c) Get movies by Director name using static methods
+    getMoviesByDirector: (_, { director_name }) =>
+      MovieModel.getByDirectorName(director_name),
+  },
+
+  Mutation: {
+    // a) Insert new movie
+    addMovie: (_, { movie }) => MovieModel.add(movie),
+
+    // b) Update movie
+    updateMovie: (_, { id, movie }) => MovieModel.update(id, movie),
+
+    // c) Delete movie by ID
+    deleteMovie: (_, { id }) => MovieModel.deleteById(id),
+  },
+};
+
+module.exports = resolvers;
