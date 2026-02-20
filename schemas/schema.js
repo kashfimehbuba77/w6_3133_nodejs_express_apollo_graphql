47afbc26cfff91e8256e5c14a86223f4af9925a6
Kashfi <kashfimehbuba71@gmail.com>
2026-02-20 04:25:37 -0500

    graphql schema

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,1 +1,41 @@
 import { gql } from 'graphql-tag';
+// graphql/schema.js
+const typeDefs = `#graphql
+  type Movie {
+    id: ID!
+    name: String!
+    director_name: String!
+    production_house: String!
+    release_date: String!
+    rating: Float!
+  }
+
+  input MovieInput {
+    name: String!
+    director_name: String!
+    production_house: String!
+    release_date: String!
+    rating: Float!
+  }
+
+  input MovieUpdateInput {
+    name: String
+    director_name: String
+    production_house: String
+    release_date: String
+    rating: Float
+  }
+
+  type Query {
+    getAllMovies: [Movie!]!
+    getMovieById(id: ID!): Movie
+    getMoviesByDirector(director_name: String!): [Movie!]!
+  }
+
+  type Mutation {
+    addMovie(movie: MovieInput!): Movie!
+    updateMovie(id: ID!, movie: MovieUpdateInput!): Movie!
+    deleteMovie(id: ID!): Boolean!
+  }
+`;
+module.exports = typeDefs;
